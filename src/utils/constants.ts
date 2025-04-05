af0a854a47a34c6ebd54a1fa9c3249e23a14efe8
archish <archish@archishs-MacBook-Pro.local>
2025-04-05 12:42:27 +0530

    Update: Updated API base url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://dev-api.lendingbridge.co.uk";
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://dev-api.lendingbridge.co.uk";
+// export const BASE_URL = "http://localhost:8080";
 
 export const ABOUT_ITEMS = [
   {
