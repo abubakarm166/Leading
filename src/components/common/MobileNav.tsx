1f502ea198d4998e1cbe111bad6c5a104cfac5c5
archish <archish@archishs-MacBook-Pro.local>
2025-04-12 15:59:53 +0530

    Add: Added a back option for sub menu in mobile

--- src/components/common/MobileNav.tsx
+++ src/components/common/MobileNav.tsx
@@ -3,10 +3,25 @@ import useNavStore from "@/utils/store/nav";
 import { motion } from "motion/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Reveal from "./Reveal";
+import { PRODUCTS } from "@/utils/constants";
+
+const WORK_OPTIONS = [
+  { title: "Blogs", href: "/blogs" },
+  { title: "Case Studies", href: "/case-studies" },
+];
+
+const BROKER_OPTIONS = [
+  { title: "Intermediaries", href: "/broker" },
+  { title: "Resources", href: "/resources" },
+];
 
 const MobileNav = () => {
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+  const [menuItemSelected, setMenuItemSelected] = useState<
+    "product" | "work" | "broker" | ""
+  >("");
   const isNavOpen = useNavStore((state) => state.isOpen);
   const onNavChange = useNavStore((state) => state.onChange);
 
@@ -15,6 +30,57 @@ const MobileNav = () => {
   const handleOnClickItem = (link: string) => {
     onNavChange(false);
     router.push(link);
+  };
+
+  const getSubMenu = () => {
+    return (
+      <div className="flex-col items-center space-y-[30px] mt-[130px]">
+        {menuItemSelected === "product"
+          ? PRODUCTS.map((item) => (
+              <p
+                key={item.id}
+                className="font-semibold font-league-spartan text-[25px]"
+                onClick={() => {
+                  setMenuItemSelected("");
+                  setIsSubMenuOpen(false);
+                  onNavChange(false);
+                  router.push(`/product/${item.id}`);
+                }}
+              >
+                {item.title}
+              </p>
+            ))
+          : menuItemSelected === "work"
+            ? WORK_OPTIONS.map((item) => (
+                <p
+                  key={item.title}
+                  className="font-semibold font-league-spartan text-[25px]"
+                  onClick={() => {
+                    setMenuItemSelected("");
+                    setIsSubMenuOpen(false);
+                    onNavChange(false);
+                    router.push(item.href);
+                  }}
+                >
+                  {item.title}
+                </p>
+              ))
+            : BROKER_OPTIONS.map((item) => (
+                <p
+                  key={item.title}
+                  className="font-semibold font-league-spartan text-[25px]"
+                  onClick={() => {
+                    setMenuItemSelected("");
+                    setIsSubMenuOpen(false);
+                    onNavChange(false);
+                    router.push(item.href);
+                  }}
+                >
+                  {item.title}
+                </p>
+              ))}
+      </div>
+    );
   };
 
   useEffect(() => {
@@ -46,69 +112,108 @@ const MobileNav = () => {
             height={46}
             alt="close"
             className="w-[26px] h-[46px] object-contain absolute top-[30px] right-[40px]"
-            onClick={() => onNavChange(false)}
+            onClick={() => {
+              setMenuItemSelected("");
+              setIsSubMenuOpen(false);
+              onNavChange(false);
+            }}
           />
+          {isSubMenuOpen && (
+            <Image
+              src="/svg/arrow-left.svg"
+              width={14}
+              height={24}
+              className="w-10 h-10 object-cover absolute top-[30px] left-10"
+              alt="arrow-left"
+              onClick={() => {
+                setIsSubMenuOpen(false);
+                setMenuItemSelected("");
+              }}
+            />
+          )}
           <div className="h-full flex flex-col items-center justify-between">
-            <div className="flex flex-col space-y-12 mt-[130px]">
-              <Reveal>
-                <div className="flex flex-row items-center space-x-4">
-                  <p className="font-semibold font-league-spartan text-[25px]">
-                    Our Product
+            {isSubMenuOpen ? (
+              getSubMenu()
+            ) : (
+              <div className="flex flex-col space-y-12 mt-[130px]">
+                <Reveal>
+                  <div
+                    className="flex flex-row items-center space-x-4"
+                    onClick={() => {
+                      setMenuItemSelected("product");
+                      setIsSubMenuOpen(true);
+                    }}
+                  >
+                    <p className="font-semibold font-league-spartan text-[25px]">
+                      Our Product
+                    </p>
+                    <Image
+                      src="/svg/chevron-down-black.svg"
+                      width={11}
+                      height={7}
+                      alt="chevron-black"
+                      className="w-[11px] h-[7px] object-cover"
+                    />
+                  </div>
+                </Reveal>
+                <Reveal delay={0.2}>
+                  <div
+                    className="flex flex-row items-center space-x-4"
+                    onClick={() => {
+                      setMenuItemSelected("work");
+                      setIsSubMenuOpen(true);
+                    }}
+                  >
+                    <p className="font-semibold font-league-spartan text-[25px]">
+                      Our Work
+                    </p>
+                    <Image
+                      src="/svg/chevron-down-black.svg"
+                      width={11}
+                      height={7}
+                      alt="chevron-black"
+                      className="w-[11px] h-[7px] object-cover"
+                    />
+                  </div>
+                </Reveal>
+                <Reveal delay={0.4}>
+                  <p
+                    className="font-semibold font-league-spartan text-[25px]"
+                    onClick={() => handleOnClickItem("/team")}
+                  >
+                    Team
                   </p>
-                  <Image
-                    src="/svg/chevron-down-black.svg"
-                    width={11}
-                    height={7}
-                    alt="chevron-black"
-                    className="w-[11px] h-[7px] object-cover"
-                  />
-                </div>
-              </Reveal>
-              <Reveal delay={0.2}>
-                <div className="flex flex-row items-center space-x-4">
-                  <p className="font-semibold font-league-spartan text-[25px]">
-                    Our Work
+                </Reveal>
+                <Reveal delay={0.6}>
+                  <div
+                    className="flex flex-row items-center space-x-4"
+                    onClick={() => {
+                      setMenuItemSelected("broker");
+                      setIsSubMenuOpen(true);
+                    }}
+                  >
+                    <p className="font-semibold font-league-spartan text-[25px]">
+                      Intermediaries
+                    </p>
+                    <Image
+                      src="/svg/chevron-down-black.svg"
+                      width={11}
+                      height={7}
+                      alt="chevron-black"
+                      className="w-[11px] h-[7px] object-cover"
+                    />
+                  </div>
+                </Reveal>
+                <Reveal delay={0.8}>
+                  <p
+                    className="font-semibold font-league-spartan text-[25px]"
+                    onClick={() => handleOnClickItem("/contact-us")}
+                  >
+                    Contact Us
                   </p>
-                  <Image
-                    src="/svg/chevron-down-black.svg"
-                    width={11}
-                    height={7}
-                    alt="chevron-black"
-                    className="w-[11px] h-[7px] object-cover"
-                  />
-                </div>
-              </Reveal>
-              <Reveal delay={0.4}>
-                <p
-                  className="font-semibold font-league-spartan text-[25px]"
-                  onClick={() => handleOnClickItem("/team")}
-                >
-                  Team
-                </p>
-              </Reveal>
-              <Reveal delay={0.6}>
-                <div className="flex flex-row items-center space-x-4">
-                  <p className="font-semibold font-league-spartan text-[25px]">
-                    Intermediaries
-                  </p>
-                  <Image
-                    src="/svg/chevron-down-black.svg"
-                    width={11}
-                    height={7}
-                    alt="chevron-black"
-                    className="w-[11px] h-[7px] object-cover"
-                  />
-                </div>
-              </Reveal>
-              <Reveal delay={0.8}>
-                <p
-                  className="font-semibold font-league-spartan text-[25px]"
-                  onClick={() => handleOnClickItem("/contact-us")}
-                >
-                  Contact Us
-                </p>
-              </Reveal>
-            </div>
+                </Reveal>
+              </div>
+            )}
             <div className="mb-[30px] flex flex-col items-center">
               <Image
                 src="/svg/logo-mobile.svg"
